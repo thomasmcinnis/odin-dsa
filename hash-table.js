05ef1296ac64ce89a31a4415a5c20620cd6f22ad
Thomas McInnis <hey@thomasmcinnis.com>
2024-02-29 23:11:33 +1100

    Basic hashmap functionality

--- hash-table.js
+++ hash-table.js
@@ -123,6 +123,7 @@ class LinkedList {
 }
 
 class HashMap {
+  // TODO: Make some load factor function to adjust size of array
   #loadFactor = 0.75;
   #capacity = 16;
 
@@ -132,73 +133,142 @@ class HashMap {
     let hashCode = 0;
 
     const prime = 31;
-    for (let i = 0; i < array.length; i++) {
-      hashCode = prime * hashCode + key.charCode;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = prime * hashCode + key.charCodeAt(i);
     }
 
-    return hashCode;
-  }
+    const index = hashCode % this.#capacity;
 
-  // A utility to overcome the fact array in javascript is really an array list
-  #checkIndex(index) {
-    if (index < 0 || index >= this.#buckets.length) {
-      throw new Error("Trying to access out of range bucket");
+    if (index < 0 || index >= this.#capacity) {
+      throw new Error("Out of range");
     }
     return index;
   }
 
   set(key, val) {
-    // will run the key through the hash, then go to that loc in the array
     const index = this.#hash(key);
-    const bucket = this.#buckets[index];
 
-    // if there is list at the bucket
-    if (bucket !== null) {
-      // check if the key is there, if it is update value
-      const nodeIndex = bucket.indexOfKey(key);
-      if (nodeIndex) {
-        bucket.updateAt(nodeIndex, val);
-        return;
-      }
-      // otherwise just prepend a new node
-      bucket.prepend(key, val);
+    if (this.#buckets[index] == null) {
+      this.#buckets[index] = new LinkedList();
+      this.#buckets[index].prepend(key, val);
       return;
     }
+    const bucket = this.#buckets[index];
 
-    bucket = new LinkedList();
-    bucket.prepend(key, val);
+    const nodeIndex = bucket.indexOfKey(key);
+    if (nodeIndex !== null) {
+      bucket.updateAt(nodeIndex, val);
+    } else {
+      bucket.prepend(key, val);
+    }
   }
 
   get(key) {
-    // return val or null
+    const index = this.#hash(key);
+    const bucket = this.#buckets[index];
+
+    if (bucket == null) return null;
+
+    const nodeIndex = bucket.indexOfKey(key);
+    if (nodeIndex == null) return null;
+
+    const node = bucket.getAt(nodeIndex);
+    return node.value;
   }
 
   has(key) {
     // return true or false
+    const index = this.#hash(key);
+    const bucket = this.#buckets[index];
+
+    if (bucket == null) return false;
+
+    const nodeIndex = bucket.indexOfKey(key);
+    if (nodeIndex == null) return false;
+
+    return true;
   }
 
   remove(key) {
     // if found, remove and return true, else false
+    const index = this.#hash(key);
+    const bucket = this.#buckets[index];
+
+    if (bucket == null) return false;
+
+    const nodeIndex = bucket.indexOfKey(key);
+    if (nodeIndex == null) return false;
+
+    bucket.removeAt(nodeIndex);
+    return true;
   }
 
   length() {
     // return the number of stored keys
+    let length = 0;
+
+    this.#buckets.forEach((bucket) => {
+      if (bucket) {
+        length += bucket.size();
+      }
+    });
+
+    return length;
   }
 
   clear() {
     // remove all the stored keys in the hash map
+    this.#buckets.fill(undefined);
   }
 
   keys() {
     // return an array containing all keys
+    let arr = [];
+
+    this.#buckets.forEach((bucket) => {
+      if (bucket && bucket.head) {
+        let curr = bucket.head;
+        while (curr.next) {
+          arr.push(curr.key);
+        }
+        arr.push(curr.key);
+      }
+    });
+    return arr;
   }
 
   values() {
     // return array of all the values
+    let arr = [];
+
+    this.#buckets.forEach((bucket) => {
+      if (bucket && bucket.head) {
+        let curr = bucket.head;
+        while (curr.next) {
+          arr.push(curr.value);
+        }
+        arr.push(curr.value);
+      }
+    });
+    return arr;
   }
 
   entries() {
     // returns each key/val pair in an array
-    // [[key, val], [key, val]]
+    let arr = [];
+
+    this.#buckets.forEach((bucket) => {
+      if (bucket && bucket.head) {
+        let curr = bucket.head;
+        while (curr.next) {
+          arr.push([curr.key, curr.value]);
+        }
+        arr.push([curr.key, curr.value]);
+      }
+    });
+
+    return arr;
   }
 }
+
+const hashMap = new HashMap();
